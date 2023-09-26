09a52ac17e3b4484517f273d4b7927e8e63c6397
Pedro Sebinelli de Godoy Oriani <TRPEDORI@traumec.com.br>
2023-09-26 16:30:29 -0300

    Feature: Aulas 3 e 4

---
+++ aula03/tests/app.test.ts
@@ -0,0 +1,18 @@
+describe("maths Tests", () => {
+    it("Sum", () => {
+        expect(2+2).toBe(4)
+    });
+
+    it("Sub", () => {
+        expect(2-2).toBe(0)
+    });
+
+    it("Mul", () => {
+        expect(2*2).toBe(4)
+    });
+
+    it("Div", () => {
+        expect(2/2).toBe(1)
+    });
+})
+
